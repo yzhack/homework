cc85872d8a447926a6b8555e59d6bdb3ab3bf614
xiao_jian <xiao_jian@hunliji.com>
2021-04-25 23:27:42 +0800

    week2 homework

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module homework
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.7.1
+require (
+	github.com/gin-gonic/gin v1.7.1
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
+
 import (
 	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
+	"github.com/pkg/errors"
 )
-func main() {
-	r := gin.Default()
+
+var db *sql.DB
+
+func init() {
 	db, err := sql.Open("mysql",
 		"user:password@tcp(127.0.0.1:3306)/hello")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer db.Close()
-	rows, err := db.Query("select id, name from users where id = ?", 1)
-	if err==sql.ErrNoRows{
-
+}
+func main() {
+	r := gin.Default()
+	if err := queryOne(); err != nil {
+		fmt.Printf("fatal:%+v\n", err)
 	}
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func queryOne() error {
+	_, err := db.Query("select id, name from users where id = ?", 1)
+	if err != nil {
+		//需要用wrap进行包装
+		//原因 1.是自己的业务应用 可以并非基础库和第三方标准库  2.不是自己主动产生的错误 3.database/sql 是标准库 所以需要包装
+		return errors.Wrap(err, "no result")
+	}
+	return nil
 }
