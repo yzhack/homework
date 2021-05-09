0e993c713f0f5573d2c1233a2739bf9147615e9d
xiao_jian <xiao_jian@hunliji.com>
2021-05-09 22:00:24 +0800

    finish week3

--- main.go
+++ main.go
@@ -1,41 +1,67 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/pkg/errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-var db *sql.DB
-
-func init() {
-	db, err := sql.Open("mysql",
-		"user:password@tcp(127.0.0.1:3306)/hello")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-}
 func main() {
 	r := gin.Default()
-	if err := queryOne(); err != nil {
-		fmt.Printf("fatal:%+v\n", err)
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	done := make(chan error, 2)
+	stop := make(chan struct{})
+	signalChan := make(chan os.Signal)
+	go func() {
+		done <- serve("127.0.0.1:8080", r, stop, &gin.Context{}, signalChan)
+	}()
+	go func() {
+		done <- serve("127.0.0.1:8001", r, stop, &gin.Context{}, signalChan)
+	}()
+
+	go func() {
+		signal.Notify(signalChan, syscall.SIGINT, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGHUP)
+	}()
+	var stopped bool
+	for i := 0; i < cap(done); i++ {
+		if err := <-done; err != nil {
+			fmt.Println("error:%v", err)
+		}
+		if !stopped {
+			stopped = true
+			close(stop)
+		}
 	}
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
-func newServer(){
-
-}
-
-func queryOne() error {
-	_, err := db.Query("select id, name from users where id = ?", 1)
-	if err != nil {
-		//需要用wrap进行包装
-		//原因 1.是自己的业务应用 可以并非基础库和第三方标准库  2.不是自己主动产生的错误 3.database/sql 是标准库 所以需要包装
-		return errors.Wrap(err, "no result")
+func serve(addr string, r *gin.Engine, stop chan struct{}, ctx *gin.Context, signal chan os.Signal) (err error) {
+	server := &http.Server{
+		Addr:    addr,
+		Handler: r,
 	}
-	return nil
+	go func() {
+		////老师 为什么我这样写 测试下来 只能关闭一个server
+		//select {
+		//case <-stop:
+		//case <-signal:
+		//	_ = server.Shutdown(ctx)
+		//}
+
+		// 这样写就能正常关闭 2个server
+		select {
+		case <-stop:
+			_ = server.Shutdown(ctx)
+		case <-signal:
+			_ = server.Shutdown(ctx)
+		}
+	}()
+	return server.ListenAndServe()
 }
