cc76fa7d75e0698ebdd3f2428e6ae2037473bd1c
xiao_jian <xiao_jian@hunliji.com>
2021-05-09 17:08:51 +0800

    ready

--- main.go
+++ main.go
@@ -26,6 +26,10 @@ func main() {
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
+func newServer(){
+
+}
+
 func queryOne() error {
 	_, err := db.Query("select id, name from users where id = ?", 1)
 	if err != nil {
