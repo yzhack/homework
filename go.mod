44bc14a6bdbf06a584051f6001387cd6ff3524d1
xiao_jian <xiao_jian@hunliji.com>
2021-05-16 11:14:11 +0800

    xiugai2

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/sync/errgroup"
+	_ "golang.org/x/sync/errgroup"
 	"net/http"
 	"os"
 	"os/signal"
@@ -11,57 +12,43 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	http.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "pong")
 	})
-	done := make(chan error, 2)
-	stop := make(chan struct{})
-	signalChan := make(chan os.Signal)
-	go func() {
-		done <- serve("127.0.0.1:8080", r, stop, &gin.Context{}, signalChan)
-	}()
-	go func() {
-		done <- serve("127.0.0.1:8001", r, stop, &gin.Context{}, signalChan)
-	}()
+	signalChan := make(chan os.Signal, 1)
+	g, ctx := errgroup.WithContext(context.Background())
+	g.Go(func() error {
+		return newServer(":9090", nil).ListenAndServe()
+	})
+	g.Go(func() error {
+		return newServer(":9091", nil).ListenAndServe()
+	})
 
-	go func() {
-		signal.Notify(signalChan, syscall.SIGINT, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGHUP)
-	}()
-	var stopped bool
-	for i := 0; i < cap(done); i++ {
-		if err := <-done; err != nil {
-			fmt.Println("error:%v", err)
+	//注册信号量
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGHUP)
+
+	//老师 为啥我这样 听不掉server进程
+	g.Go(func() error {
+		for {
+			select {
+			case <-ctx.Done():
+			case <-signalChan:
+				if err := newServer("9090", nil).Shutdown(ctx); err != nil {
+					return err
+				}
+				if err := newServer("9091", nil).Shutdown(ctx); err != nil {
+					return err
+				}
+			}
 		}
-		if !stopped {
-			stopped = true
-			close(stop)
-		}
-	}
+	})
+	err := g.Wait()
+	fmt.Printf("err:%s", err.Error())
 }
 
-func serve(addr string, r *gin.Engine, stop chan struct{}, ctx *gin.Context, signal chan os.Signal) (err error) {
-	server := &http.Server{
+func newServer(addr string, handler http.Handler) (server *http.Server) {
+	return &http.Server{
 		Addr:    addr,
-		Handler: r,
+		Handler: handler,
 	}
-	go func() {
-		////老师 为什么我这样写 测试下来 只能关闭一个server
-		//select {
-		//case <-stop:
-		//case <-signal:
-		//	_ = server.Shutdown(ctx)
-		//}
-
-		// 这样写就能正常关闭 2个server
-		select {
-		case <-stop:
-			_ = server.Shutdown(ctx)
-		case <-signal:
-			_ = server.Shutdown(ctx)
-		}
-	}()
-	return server.ListenAndServe()
 }
