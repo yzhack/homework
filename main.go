5a33d0168a1d1914715e9fe3d43e5433baf2b5df
xiao_jian <xiao_jian@hunliji.com>
2021-05-23 23:46:51 +0800

    ss

--- main.go
+++ main.go
@@ -15,30 +15,27 @@ func main() {
 	http.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(writer, "pong")
 	})
+	serversChan := make(chan *http.Server, 2)
 	signalChan := make(chan os.Signal, 1)
 	g, ctx := errgroup.WithContext(context.Background())
 	g.Go(func() error {
-		return newServer(":9090", nil).ListenAndServe()
+		return newServer(":9090", nil, serversChan).ListenAndServe()
 	})
 	g.Go(func() error {
-		return newServer(":9091", nil).ListenAndServe()
+		return newServer(":9091", nil, serversChan).ListenAndServe()
 	})
 
 	//注册信号量
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGHUP)
 
-	//老师 为啥我这样 听不掉server进程
 	g.Go(func() error {
 		for {
 			select {
 			case <-ctx.Done():
+				fmt.Printf("done len:%d",len(serversChan))
 			case <-signalChan:
-				if err := newServer("9090", nil).Shutdown(ctx); err != nil {
-					return err
-				}
-				if err := newServer("9091", nil).Shutdown(ctx); err != nil {
-					return err
-				}
+				fmt.Printf("signalChan len:%d",len(serversChan))
+				return stopServer(ctx, serversChan)
 			}
 		}
 	})
@@ -46,9 +43,20 @@ func main() {
 	fmt.Printf("err:%s", err.Error())
 }
 
-func newServer(addr string, handler http.Handler) (server *http.Server) {
-	return &http.Server{
+func newServer(addr string, handler http.Handler, servers chan *http.Server) (server *http.Server) {
+	s := &http.Server{
 		Addr:    addr,
 		Handler: handler,
 	}
+	servers <- s
+	return s
+}
+
+func stopServer(ctx context.Context, servers chan *http.Server) (err error) {
+	for v := range servers {
+		if err := v.Shutdown(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
 }
